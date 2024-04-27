7a6f5b958804308c89792f5551c890eea159d541
Omar-abftah <148388901+Omar-abftah@users.noreply.github.com>
2024-04-27 20:31:01 +0300

    Update user.js

--- 20220218_project2_NehalAhmedAbdelbaqi_phase2/javaScript/user.js
+++ 20220218_project2_NehalAhmedAbdelbaqi_phase2/javaScript/user.js
@@ -5,31 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function renderBooks(obj) {
-    let divForBook = document.querySelector('.wrapper');
+    let tempDiv = document.querySelector('.wrapper');
     let id;
     obj.map((o) => {
-        const booksDiv = document.querySelector('.c2');
         let s = '';
         s += `<div class="c2">`;
-        s += `<br><h3>${o.title}</h3><br>`;
         s += `<img src="${o.cover}"/>`;
+        s += `<h3>${o.title}</h3>`;
         s += '<div class="book-details">'
         s += `<p><b>Author</b>: ${o.author}<br/></p>`;
         s += `<p><b>Publisher</b>: ${o.publisher}<br/></p>`;
         s += `<p><b>Publishing Year</b>: ${o.publishedYear}<br/></p>`;
         s += `<p><b>ISBN</b>: ${o.isbn}<br/></p>`;
         s += `<p><b>Description</b>: ${o.description}<br/></p>`;
+        s += `<p><b>Quantity</b>: ${o.quantity}<br/></p>`;
         if (o.quantity > 0) {
             s += `<p><b>Available to borrow<br/></p>`;
+            s += `<a href="borrow_book.html" class="btn">Borrow</a>`;
         }
         else {
             s += `<p><b>Currently not available<br/></p>`;
         }
-        s += '<a href="borrow_book.html" class="btn">borrow</a>';
+        s += '</div>';
         s += '</div>'
-        s += '</div>'
-        divForBook.innerHTML = s;
-        booksDiv.appendChild(divForBook);
+        tempDiv.innerHTML += s;
     });
 }
 const searchBtn = document.querySelector('#search-btn');
