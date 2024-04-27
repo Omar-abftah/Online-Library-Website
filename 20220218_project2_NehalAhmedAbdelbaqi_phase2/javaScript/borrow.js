5875992d52ac44aad313e188e29bbbcdf125789f
shams229 <147387782+shams229@users.noreply.github.com>
2024-04-27 19:10:31 +0300

    Update borrow.js

--- 20220218_project2_NehalAhmedAbdelbaqi_phase2/javaScript/borrow.js
+++ 20220218_project2_NehalAhmedAbdelbaqi_phase2/javaScript/borrow.js
@@ -1,25 +1,34 @@
-document.getElementById("form").addEventListener("submit", 
-function(event) {
-    event.preventDefault();
-    var borrowerName = document.getElementById("borrowerName").value;
-    var email = document.getElementById("email").value;
-    var telephone = document.getElementById("telephone").value;
-    var nationalId = document.getElementById("national-id").value;
-    var bookTitle = document.getElementById("book-title").value;
-    var startDate = document.getElementById("start_date").value;
-    var dueDate = document.getElementById("due_date").value;
-    var borrowedBooks = JSON.parse(localStorage.getItem("borrowedBooks")) || [];
-    var newBook = {
-      "bookTitle": bookTitle,
-      "borrowerName":borrowerName,
-      "email": email,
-      "telephone": telephone,
-      "nationalId": nationalId,
-      "startDate": startDate,
-      "dueDate": dueDate
-    };
-    borrowedBooks.push(newBook);
-    localStorage.setItem("borrowedBooks", JSON.stringify(borrowedBooks));
-    document.getElementById("form").reset();
-    alert('Book borrowed successfully!');
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  function createBorrowedBook(bookTitle, borrowerName, email, telephone, nationalId, startDate, dueDate) {
+      return {
+          bookTitle: bookTitle,
+          borrowerName: borrowerName,
+          email: email,
+          telephone: telephone,
+          nationalId: nationalId,
+          startDate: startDate,
+          dueDate: dueDate
+      };
+  }
+
+  const forms = document.getElementsByClassName('form');
+  for (let i = 0; i < forms.length; i++) {
+      forms[i].addEventListener('submit', function (event) {
+          event.preventDefault();
+          const borrowerName = forms[i].querySelector('#borrowerName').value;
+          const email = forms[i].querySelector('#email').value;
+          const telephone = forms[i].querySelector('#telephone').value;
+          const nationalId = forms[i].querySelector('#national-id').value;
+          const bookTitle = forms[i].querySelector('#book-title').value;
+          const startDate = forms[i].querySelector('#start_date').value;
+          const dueDate = forms[i].querySelector('#due_date').value;
+
+          const newBorrowedBook = createBorrowedBook(bookTitle, borrowerName, email, telephone, nationalId, startDate, dueDate);
+          const borrowedBooks = JSON.parse(localStorage.getItem("borrowedBooks")) || [];
+          borrowedBooks.push(newBorrowedBook);
+          localStorage.setItem("borrowedBooks", JSON.stringify(borrowedBooks));
+          forms[i].reset();
+          alert('Book borrowed successfully!');
+      });
+  }
+});
